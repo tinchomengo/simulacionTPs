5f6881d1d8f64feb58809f320f004b6a628386d0
Isauro Manuel Rodriguez <90867@sistemas.frc.utn.edu.ar>
2024-05-07 16:17:00 -0300

    Logica casi terminada

--- TP3 Simulacion/simulacion.py
+++ TP3 Simulacion/simulacion.py
@@ -24,41 +24,52 @@ def simulacion(tupla_probs):
         #Determinar Demanda
         fila[fila_usar][1][0] = round(round(random.random(),4),4)
         fila[fila_usar][1][1] = funcionBuscar(tupla_probs[0], fila[fila_usar][1][0])
-        acumulador_vendidos = fila[fila_usar][1][1]
+        acumulador_vendidos += fila[fila_usar][1][1]
         #Determinar autos y comisiones
-        for i in range(fila[fila_usar][1][1]): #Estoy utilizando la demanda solicitada para recorrer y cambiar el array de autos y comisiones
-            # Determino el tipo de auto
-            fila[fila_usar][2][i][0] = round(random.random(),4)
-            fila[fila_usar][2][i][1] = funcionBuscar(tupla_probs[1], fila[fila_usar][2][i][0]) 
-            # Determino la comision segun tipo de auto
-            if(fila[fila_usar][2][i][1] == "compacto"):
-                fila[fila_usar][3][i][0] = 0
-                fila[fila_usar][3][i][1] = 250
-            elif (fila[fila_usar][2][i][1] == "mediano"):
-                fila[fila_usar][3][i][0] = round(random.random(),4)
-                fila[fila_usar][3][i][1] = funcionBuscar(tupla_probs[2], fila[fila_usar][3][i][0])
-            elif (fila[fila_usar][2][i][1] == "de lujo"):
-                fila[fila_usar][3][i][0] = round(random.random(),4)
-                fila[fila_usar][3][i][1] = funcionBuscar(tupla_probs[3], fila[fila_usar][3][i][0])
-            # Si es una cuarta semana, se realiza el sorteo
+        if(fila[fila_usar][1][1]>0):
+            for i in range(fila[fila_usar][1][1]): #Estoy utilizando la demanda solicitada para recorrer y cambiar el array de autos y comisiones
+                # Determino el tipo de auto
+                fila[fila_usar][2][i][0] = round(random.random(),4)
+                fila[fila_usar][2][i][1] = funcionBuscar(tupla_probs[1], fila[fila_usar][2][i][0]) 
+                # Determino la comision segun tipo de auto
+                if(fila[fila_usar][2][i][1] == "compacto"):
+                    fila[fila_usar][3][i][0] = 0
+                    fila[fila_usar][3][i][1] = 250
+                elif (fila[fila_usar][2][i][1] == "mediano"):
+                    fila[fila_usar][3][i][0] = round(random.random(),4)
+                    fila[fila_usar][3][i][1] = funcionBuscar(tupla_probs[2], fila[fila_usar][3][i][0])
+                elif (fila[fila_usar][2][i][1] == "de lujo"):
+                    fila[fila_usar][3][i][0] = round(random.random(),4)
+                    fila[fila_usar][3][i][1] = funcionBuscar(tupla_probs[3], fila[fila_usar][3][i][0])
+                    
+                
+
+
+        # Determinar comision total de la fila
+        for i in range(4):
             fila[fila_usar][5] += fila[fila_usar][3][i][1]
-            if(semana%4 == 0):
-                if(acumulador_vendidos >=4):
-                    fila[fila_usar][4][0] = round(random.random(),4)
-                    fila[fila_usar][4][1] = funcionBuscar(tupla_probs[4], fila[fila_usar][4][0])
-                    if(fila[fila_usar][4][1] == "Gana el sorteo"):
-                        fila[fila_usar][5] += 5000
-                acumulador_vendidos = 0
-            #Comision Acumulado
-            fila[fila_usar][6] = (fila[fila_no_usar][6] + fila[fila_usar][5])
-            #Comision Promedio
-            fila[fila_usar][7] = fila[fila_usar][6]/fila[fila_usar][0]
+
+
+        # Si es una cuarta semana, se realiza el sorteo      
+        if((fila[fila_usar][0])%4 == 0):
+            if(acumulador_vendidos >=4):
+                fila[fila_usar][4][0] = round(random.random(),4)
+                fila[fila_usar][4][1] = funcionBuscar(tupla_probs[4], fila[fila_usar][4][0])
+                if(fila[fila_usar][4][1] == "Gana el sorteo"):
+                    fila[fila_usar][5] += 5000
+            else:
+                fila[fila_usar][4][1] = "No aplica"
+            acumulador_vendidos = 0
+        #Comision Acumulado
+        fila[fila_usar][6] = (fila[fila_no_usar][6] + fila[fila_usar][5])
+        #Comision Promedio
+        fila[fila_usar][7] = fila[fila_usar][6]/fila[fila_usar][0]
 
         if((tupla_probs[5][1]+tupla_probs[5][0] > semana >= tupla_probs[5][0]) or semana == tupla_probs[6]-1):
             filas_guardadas.append(copy.copy(fila[fila_usar]))
             print(fila[fila_usar])
 
-        reiniciar_fila(fila[fila_no_usar])
+        fila[fila_no_usar] = reiniciar_fila()
 
         if(fila_usar == 0):
             fila_usar = 1
@@ -80,5 +91,5 @@ def funcionBuscar(tupla_determinar, rnd):
                 return tupla_determinar[i][1]
         acumulador += tupla_determinar[i][0]/100
 
-def reiniciar_fila(fila):
-    fila = [0,[0,0],[[0,""],[0,""],[0,""],[0,""]],[[0,0],[0,0],[0,0],[0,0]],[0,""],0,0,0]
+def reiniciar_fila():
+    return [0,[0,0],[[0,""],[0,""],[0,""],[0,""]],[[0,0],[0,0],[0,0],[0,0]],[0,""],0,0,0]
